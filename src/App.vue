07477bc2ec4612bd6acb4ea7b6144b4c92a4f8d2
samuelayo <ayoogundipe2005@gmail.com>
2017-01-30 10:49:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  <!-- the router outlet, where all matched components would ber viewed -->
+  <router-link v-bind:to="'/'">Home</router-link>
+<router-link v-bind:to="'/about'">About</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<!-- styling for the component -->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//import the vue instance
+import Vue from 'vue'
+//import the App component
+import App from './App'
+//import the vue router
+import VueRouter from 'vue-router'
+//tell vue to use the router
+Vue.use(VueRouter)
+/* eslint-disable no-new */
+//import the hello component
+import Hello from './components/Hello'
+//import the about component
+import About from './components/About'
+//define your routes
+const routes = [
+	//route for the home route of the webpage
+	{ path: '/', component: Hello },
+	//route for the about route of the webpage
+	{ path: '/about', component: About }
+]
+
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
+})
+//instatinat the vue instance
+new Vue({
+	//define the selector for the root component
+  el: '#app',
+  //pass the template to the root component
+  template: '<App/>',
+  //declare components that the root component can access
+  components: { App },
+  //pass in the router to the vue instance
+  router
+}).$mount('#app')//mount the router on the app
